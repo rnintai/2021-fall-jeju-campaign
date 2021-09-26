cb1f4eb046f003ab273fbac4aecc8df16a886183
rnintai <ho46825@gmail.com>
2021-09-26 14:11:55 +0900

    Fix: change page title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./assets/logo.svg";
 import "./App.css";
+import ApiTest from "./components/ApiTest";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>1st week: Kim, Min-tae</p>
+        <p>Kim, Min-tae</p>
       </header>
+      <ApiTest></ApiTest>
     </div>
   );
 }
