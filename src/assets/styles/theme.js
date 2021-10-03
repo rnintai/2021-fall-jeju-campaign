242fa44e9dddd0fe153649d915e58f7dd2e05b08
rnintai <ho46825@gmail.com>
2021-10-03 17:52:56 +0900

    Feat: Add theme fontsize

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -8,8 +8,17 @@ const color = {
   light_green: "#b8d563",
 };
 
+const fontSize = {
+  l: "1.4rem",
+  ml: "1.2rem",
+  m: "1rem",
+  ms: "0.8rem",
+  s: "0.6rem",
+};
+
 const theme = {
   color,
+  fontSize,
 };
 
 export default theme;
