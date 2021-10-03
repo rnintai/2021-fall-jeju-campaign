dce7f788e3e20f2623df70691125450b0bb504b9
rnintai <ho46825@gmail.com>
2021-10-03 17:10:55 +0900

    Fix: Change padding of background wrap

--- src/components/styled/Home.js
+++ src/components/styled/Home.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Background = styled.main`
   position: relative;
-  padding: 0 20%;
+  padding: 0 27%;
   background-color: #fef2c6;
   min-width: 500px;
 
   @media screen and (max-width: 768px) {
-    padding: 0;
+    padding: 0 7%;
   }
 
   transition: padding 0.5s ease-in-out;
