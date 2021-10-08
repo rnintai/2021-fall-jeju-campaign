f55ed5fb96411be185af71de174d13e6a652029a
rnintai <ho46825@gmail.com>
2021-10-08 19:12:46 +0900

    refactor: change function name

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,28 +1,27 @@
+import axios from "axios";
 import React, { useState } from "react";
-
-import searchApi from "../api/searchApi"
 
 import { LocationWrap } from "./styled/Location";
 
 function Locations() {
   let [locations, setLocations] = useState([]);
 
-  // function searchApi() {
-  //   const url = process.env.REACT_APP_BASEURL;
-  //   axios
-  //     .get(url + "/locations")
-  //     .then(function (response) {
-  //       setLocations(response.data.locations);
-  //     })
-  //     .catch(function (error) {
-  //       console.log("실패");
-  //     });
-  // }
+  function apiLocation() {
+    const url = process.env.REACT_APP_BASEURL;
+    axios
+      .get(url + "/locations")
+      .then(function (response) {
+        setLocations(response.data.locations);
+      })
+      .catch(function (error) {
+        console.log("실패");
+      });
+  }
 
   if (locations !== undefined && locations.length > 0) {
     return <LocationWrap locations={locations}></LocationWrap>;
   } else {
-    searchApi();
+    apiLocation();
     return null;
   }
 }
