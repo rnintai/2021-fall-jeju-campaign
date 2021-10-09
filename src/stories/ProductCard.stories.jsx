0234320cee44ca77aec0740998ebd7ea3613b362
rnintai <ho46825@gmail.com>
2021-10-09 20:44:54 +0900

    Feat: Create story about ProductCard Component

---
+++ src/stories/ProductCard.stories.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ProductCard } from "../components/styled/ProductCard";
+
+const FlexBox = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export default {
+  title: "Example/ProductCard",
+  component: ProductCard,
+  argTypes: {
+    themeColor: { control: "color" },
+  },
+};
+
+const Template = (args) => (
+  <FlexBox>
+    <ProductCard {...args} />
+    <ProductCard {...args} />
+    <ProductCard {...args} />
+    <ProductCard {...args} />
+    <ProductCard {...args} />
+    <ProductCard {...args} />
+  </FlexBox>
+);
+
+export const Example = Template.bind({});
